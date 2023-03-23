5bd361247ff2e915cdd818185a1b0d126490fd84
Xin Wen Yap <yapxinwen1608@gmail.com>
2023-03-23 13:12:27 -0400

    add brand list and detail pages

--- controllers/brandController.js
+++ controllers/brandController.js
@@ -1,6 +1,43 @@
-exports.list = (req, res) => {};
+const Brand = require('../models/brand');
+const Model = require('../models/model');
 
-exports.detail = (req, res) => {};
+exports.list = async (req, res, next) => {
+	try {
+		const [brands, counts] = await Promise.all([
+			Brand.find({}),
+			Model.aggregate([
+				{
+					$group: {
+						_id: '$brand',
+						count: { $sum: 1 }
+					},
+				},
+			]).then(data => data.reduce((map, obj) => {
+				map[obj._id.toString()] = obj.count;
+				return map;
+			}, {})),
+		]);
+
+		console.log(counts);
+
+		res.render('brand_list', { title: 'Brand List', brands, counts });
+	} catch (err) {
+		return next(err);
+	}
+};
+
+exports.detail = async (req, res, next) => {
+	try {
+		const [brand, brandModels] = await Promise.all([
+			Brand.findById(req.params.id),
+			Model.find({ brand: req.params.id }).populate('instrument brand'),
+		]);
+
+		res.render('brand_detail', { title: 'Brand Detail', brand, brandModels });
+	} catch (err) {
+		return next(err);
+	}
+};
 
 exports.createGET = (req, res) => {};
 
